5bc4049c6e9751d2ce36e38592f61f1e61b2fa61
Brad Prevost <brad.prevost@yahoo.com>
2023-11-19 21:14:11 -0600

    Keras CIFAR-10 demo.

--- dlcv/keras_cifar10_demo.py
+++ dlcv/keras_cifar10_demo.py
@@ -7,13 +7,13 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
-# load the MNIST dataset of 70,000 hand-written digits
-# 60,000 for training and 10,000 for testing
-((trainX, trainY), (testX, testY)) = tf.keras.datasets.mnist.load_data()
+# load the CIFAR-10 dataset of 60,000 images of 10 classes
+# 50,000 for training and 10,000 for testing
+((trainX, trainY), (testX, testY)) = tf.keras.datasets.cifar10.load_data()
 
-# flatten each 28x28x1 image to a simple list of 28x28=784 pixels
-trainX = trainX.reshape((trainX.shape[0], 28 * 28 * 1))
-testX = testX.reshape((testX.shape[0], 28 * 28 * 1))
+# flatten each 32x32x3 image to a simple list of 32x32x3=3072 pixels
+trainX = trainX.reshape((trainX.shape[0], 32 * 32 * 3))
+testX = testX.reshape((testX.shape[0], 32 * 32 * 3))
 
 # scale data to the range of [0, 1]
 trainX = trainX.astype('float32') / 255.0
@@ -29,8 +29,8 @@ num_features = trainX.shape[1]
 print(f'{num_features=}')
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.InputLayer(input_shape=(num_features,))) # input layer
-model.add(tf.keras.layers.Dense(256, activation='sigmoid')) # hidden layer
-model.add(tf.keras.layers.Dense(128, activation='sigmoid')) # hidden layer
+model.add(tf.keras.layers.Dense(1024, activation='relu')) # hidden layer
+model.add(tf.keras.layers.Dense(512, activation='relu')) # hidden layer
 model.add(tf.keras.layers.Dense(10, activation='softmax')) # output layer
 model.summary()
 
@@ -38,15 +38,15 @@ model.summary()
 sgd = tf.keras.optimizers.SGD(0.01)
 
 # compile the model
-model.compile(loss="categorical_crossentropy", optimizer=sgd,  metrics=["accuracy"])
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
 
 # train the model
 EPOCHS = 100
-H = model.fit(x=trainX, y=trainY, validation_data=(testX, testY), epochs=EPOCHS, batch_size=128)
+H = model.fit(trainX, trainY, validation_data=(testX, testY), epochs=EPOCHS, batch_size=32)
 
 # evaluate the network
-predictions = model.predict(testX, batch_size=128)
-label_names = [str(x) for x in lb.classes_]
+predictions = model.predict(testX, batch_size=32)
+label_names = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
 print(label_names)
 report = classification_report(
     y_true=testY.argmax(axis=1),
@@ -65,4 +65,4 @@ plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend()
-plt.savefig('keras_mnist_demo.png')
+plt.savefig('keras_cifar10_demo.png')
